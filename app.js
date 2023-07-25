988a828178b387e4f6448074b2a766f42a5097c0
PedroPezoaRL <pedro@resgateleads.com.br>
2023-07-24 21:39:39 -0300

    Clean app.js and add new routes
    Added 2 new routes to handle technical information, and clean the API from unused routes

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const express           = require("express");
-const dotenv            = require("dotenv");
-const bodyParser        = require("body-parser");
-const client            = require("./resource/redis");
-const getChatCompletion = require("./resource/openai");
-const { 
-    includeVectors, 
-    listVectors, 
-    include, 
-    list, 
-    deleteNamespace }   = require("./resource/pinecone");
+const express              = require("express");
+const dotenv               = require("dotenv");
+const bodyParser           = require("body-parser");
+//const client               = require("./resources/redis");
+const getChatCompletion    = require("./resources/openai");
+const insertTechnicalInfo  = require("./resources/auxiliar");
 
 dotenv.config();
 
@@ -16,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
-
+/*
 app.post("/redis/users", (req, res) => {
     const { key } = req.body;
 
@@ -30,63 +25,13 @@ app.get("/redis/users", async (req, res) => {
 
     return res.status(200).json(value);
 });
-
-app.post("/pinecone/include/vectors", async (req, res) => {
-    try {
-        await includeVectors(req.body);
-
-        return res.status(201).json({ "message": "Save" });
-    } catch (error) {
-        return res.status(500).json({ "error": "Internal Server Error" });
-    }
-});
-
-app.post("/pinecone/list/vectors", async (req, res) => {
-    try {
-        const queryResponse = await listVectors(req.body);
-
-        return res.status(200).json({queryResponse});
-    } catch (error) {
-        return res.status(500).json({ "error": "Internal Server Error" });
-    }
-});
-
-app.post("/pinecone/include", async (req, res) => {
-    try {
-        await include(req.body);
-
-        return res.status(200).json({"message": "Save data"});
-    } catch (error) {
-        return res.status(500).json({ "error": "Internal Server Error" });
-    }
-});
-
-app.post("/pinecone/list", async (req, res) => {
-    try {
-        const fetchResult = await list(req.body);
-
-        return res.status(200).json({fetchResult});
-    } catch (error) {
-        return res.status(500).json({ "error": "Internal Server Error" });
-    }
-});
-
-app.delete("/pinecone/delete/:namespace", async (req, res) => {
-    try {
-        await deleteNamespace(req.params.namespace);
-
-        return res.status(200).json({"message": "Delete success"});
-    } catch (error) {
-        return res.status(500).json({ "error": "Internal Server Error" });
-    }
-});
-
+*/
 app.post("/openai/prompt", async (req, res) => {
     const { prompt } = req.body;
     try {
         const completion = await getChatCompletion(prompt);
 
-        historySizeCheck();
+        // checkHistorySize();
 
         return res.status(200).json({ completion });
     } catch (error) {
@@ -94,7 +39,27 @@ app.post("/openai/prompt", async (req, res) => {
     }
 });
 
-client.connect();
+app.get("/upload-tecnico", async (req, res) => {
+    try {
+        await insertTechnicalInfo();
+
+        return res.status(200).json({ "message": "Success" });
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao chamar a API do OpenAI" });
+    }
+});
+
+app.get("/reset-tecnico", async (req, res) => {
+    try {
+        await resetTechnicalInfo();
+
+        return res.status(200).json({ "message": "Success" });
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao chamar a API do OpenAI" });
+    }
+});
+
+//client.connect();
 
 app.listen(5000, async () => {
     console.log('SERVIDOR RODANDO PORTA 5000');
