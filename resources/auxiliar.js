fe81afb778afe079377840618e0bed44d7cdfba6
PedroPezoaRL <pedro@resgateleads.com.br>
2023-07-25 18:41:29 -0300

    Create methods to insert and update technical information vectors

--- resources/auxiliar.js
+++ resources/auxiliar.js
@@ -1,48 +1,39 @@
-const fs = require('fs').promises;
-const textToVector = require("./openai");
-const { include,deleteNamespace } = require("./pinecone");
+const fs = require('fs');
+const textToVector = require("./openai").textToVector;
+const { include, deleteNamespace } = require("./pinecone");
 
 async function insertTechnicalInfo(){
     try {
-        const chunks = textToChunks(importContent(), 50);
-        const vectors = []
-
-        chunks.forEach(element => {
-            vectors.push(textToVector(element));
-        });
-
+        const chunks = importContent().split("===");
+        var vectors = [];
+        
+        for (const chunk of chunks) {
+            let vector = await textToVector(chunk);
+            vectors.push(vector);
+        }
+        
         for (var i = 0; i < chunks.length; i++) {
             include("tec-" + i,vectors[i],"tecnico",chunks[i]);
         }
     } catch (error) {
         console.error("Erro ao subir as informações técnicas");
+        console.error(error);
         throw error;
     }
 }
 
 async function resetTechnicalInfo() {
-    deleteNamespace("tecnico");
-    insertTechnicalInfo();
+    await deleteNamespace("tecnico");
+    await insertTechnicalInfo();
 }
 
-async function importContent() {
+function importContent() {
     try {
-        return await fs.readFile('informacao_tecnico.txt', 'utf-8');;
-    } catch (error) {
-        console.error("Erro ao ler o arquivo:", error);
-        throw error;
+        return fs.readFileSync('./resources/informacao_tecnico.txt', 'utf8');
+    } catch(error) {
+        console.error("Erro ao ler o documento");
+        console.error(error);
     }
 }
 
-function textToChunks(content, amountLines) {
-    const lines = content.split('\n');
-    const chunks = [];
-
-    for (let i = 0; i < lines.length; i += amountLines) {
-        chunks.push(lines.slice(i, i + amountLines).join('\n'));
-    }
-
-    return chunks;
-}
-
-module.exports = { insertTechnicalInfo,resetTechnicalInfo};
+module.exports = { insertTechnicalInfo,resetTechnicalInfo };
