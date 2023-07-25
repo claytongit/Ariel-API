c556e1a74fb66b3cd24fddb77805399cbf0b6451
PedroPezoaRL <pedro@resgateleads.com.br>
2023-07-24 21:33:40 -0300

    Clean module from unused methods

--- resource/pinecone.js
+++ resource/pinecone.js
@@ -1,5 +1,4 @@
 const { PineconeClient } = require("@pinecone-database/pinecone");
-const axios = require("axios");
 
 const pinecone = new PineconeClient();
 
@@ -8,87 +7,28 @@ pinecone.init({
 	apiKey:      process.env.PINECONE_API_KEY
 });
 
-const index = await pinecone.Index("arielapp");
+async function include(id, vector, namespace, metadata) {
+    const index = await pinecone.Index("arielapp");
 
-async function includeVectors(reqBody) {
-    const { text, id, metaValue, name } = reqBody;
-
-    // const index = pinecone.Index("arielapp");
-
-    const response = await axios.post('https://tfhub.dev/google/universal-sentence-encoder/4?tfjs-format=tfjs', {
-        sentences: [text]
-    });
-
-    const vectors = response.data;
-    const upsertRequest = {
-        vectors: [{
-            id: id,
-            values: vectors,
-            metadata: {
-                genre: metaValue
+    await index.upsert({
+        vectors: [
+            {
+                id: id,
+                values: vector,
+                metadata: { content: metadata },
             }
-        }],
-        namespace: name,
-    };
-
-    return await index.upsert(upsertRequest);
-}
-
-async function listVectors(reqBody) {
-    const { values, metaValue, name } = reqBody;
-
-    // const index = pinecone.Index("arielapp");
-
-    const queryRequest = {
-        vector: values,
-        topK: 10,
-        includeValues: true,
-        includeMetadata: true,
-        filter: {
-            genre: { $in: [metaValue] },
-        },
-        namespace: name,
-    };
-
-    return await index.query({ queryRequest });
-}
-
-async function include(reqBody) {
-    const { vetores, namespace, metadata } = reqBody;
-
-    // const index = pinecone.Index("arielapp");
-
-    return await index.upsert({ 
-        upsertRequest: { 
-            vectors: [
-                { id: '1', values: vetores }
-            ],
-            metadata: [
-                { "exemplo": metadata }
-            ], 
-            namespace: namespace
-        }
-    });
-}
-
-async function list(reqBody) {
-    const { ids, namespace } = reqBody;
-
-    // const index = pinecone.Index("arielapp");
-
-    return await index.fetch({
-        ids,
-        namespace
+        ],
+        namespace: namespace
     });
 }
 
 async function deleteNamespace(namespace) {
-    // const index = pinecone.Index("arielapp");
-    
+    const index = await pinecone.Index("arielapp");
+
     return await index.delete1({
         deleteAll: true,
         namespace: namespace
     });
 }
 
-module.exports = { includeVectors, listVectors, include, list, deleteNamespace };
+module.exports = { include, deleteNamespace };
