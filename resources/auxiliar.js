54f469d4e70526887f6d587677d121a5ff76d963
PedroPezoaRL <pedro@resgateleads.com.br>
2023-07-28 18:38:44 -0300

    Add and update methods to send chat history to Pinecone

--- resources/auxiliar.js
+++ resources/auxiliar.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
+//const client = require("./redis");
 const { getChatCompletion, textToVector, getHistoryChat } = require("./openai");
 const { include, deleteNamespace, searchBySimilarity } = require("./pinecone");
 
+//client.connect();
+
 async function askGPT(question) {
     try {
-        const technicalInfo = await searchBySimilarity(await textToVector(question));
-        
-        return await getChatCompletion(question, technicalInfo);
+        //let historyChatRedis = await client.get('arielKey');
+        let questionVector = await textToVector(question);
+        let technicalInfo = await searchBySimilarity(questionVector);
+        let result = await getChatCompletion(question, technicalInfo, historyChatRedis);
+
+        if (result.currentTokenSize >= 16384) {
+            restartHistory(result.historyChat);
+        }
+
+        return result.answer;
     } catch (error) {
         console.error("Erro ao perguntar para o GPT");
         console.error(error);
@@ -16,6 +26,30 @@ async function askGPT(question) {
 
 function checkHistory() {
     return getHistoryChat();
+}
+
+function restartHistory(historyChatRedis) {
+    let content = "";
+    var i = 1;
+
+    for (; i < historyChatRedis.length; i = i + 2) {
+        content += historyChatRedis[i].content + "\n" + 
+                   historyChatRedis[i+1].content + "\n";
+
+        if (content.length >= 400) {
+            let vector = await textToVector(content);
+            await include(content.substring(0, 8) + i,vector,"user",content);
+
+            content = "";
+        }
+    }
+
+    if (content.length > 0) {
+        let vector = await textToVector(content);
+        await include(content.substring(0, 8) + i,vector,"user",content);
+    }
+
+    //client.set(`arielKey`, key);
 }
 
 async function insertTechnicalInfo(){
